759f4a9566aaa171cab9b7e04f0d104a3194cb0c
Frédéric DROUHIN <frederic.drouhin@gmail.com>
2021-11-29 14:39:45 +0100

    Permutation : ajout de la méthode de combinaison linéaire

--- permutation.py
+++ permutation.py
@@ -20,3 +20,21 @@ y = c
 print("Après permutation : ")
 print("\tx :", x)
 print("\ty :", y)
+
+# Permutation par combinaison linéaire
+# Ceci est une permutation linéaire avec des nombres (ici des réels)
+x = float(input("Saisir x : "))
+y = float(input("Saisir y : "))
+
+print("Avant permutation : ")
+print("\tx :", x)
+print("\ty :", y)
+
+# Permutation
+x = x + y
+y = x - y
+x = x - y
+
+print("Après permutation : ")
+print("\tx :", x)
+print("\ty :", y)
